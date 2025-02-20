7bf9d4faa054a5928504c47c929f9a95d0ddb2a8
Khiyraa <khiyraa@gmail.com>
2025-02-20 20:10:06 +0700

    reguler & single expression function

---
+++ daily/reguler_function.dart
@@ -0,0 +1,11 @@
+int hitungLuasPersegi(int sisi) {
+  // Beberapa baris kode bisa ada di sini
+  int luas = sisi * sisi;
+  return luas;
+}
+
+// Cara menggunakan
+void main() {
+  int hasil = hitungLuasPersegi(5);
+  print(hasil); // Output: 25
+}
---
+++ daily/single_expresion_function.dart
@@ -0,0 +1,8 @@
+// Single expression function untuk menghitung luas persegi
+int hitungLuasPersegi(int sisi) => sisi * sisi;
+
+// Cara menggunakan
+void main() {
+  int hasil = hitungLuasPersegi(5);
+  print(hasil); // Output: 25
+}
